57b7644aac5c92f198ca9bb83acc9b5438135877
georgiybykov <zhora.bykov@gmail.com>
2021-03-01 03:15:32 +0300

    Add accossiation expectations to user_spec for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 describe User, type: :model do
+  it { is_expected.to have_many(:questions).dependent(:nullify) }
+  it { is_expected.to have_many(:answers).dependent(:nullify) }
+
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_presence_of :password }
 end
