18d297659db615d19824aad4767b0638fdd5c588
georgiybykov <zhora.bykov@gmail.com>
2021-03-04 14:46:00 +0300

    Delete all the questions and answers for user after deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :questions, dependent: :nullify
-  has_many :answers, dependent: :nullify
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable, :confirmable
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe Question, type: :model, aggregate_failures: true do
-  it { is_expected.to belong_to(:user).optional(true) }
+  it { is_expected.to belong_to(:user).touch(true) }
   it { is_expected.to have_many(:answers).dependent(:destroy) }
 
   it { is_expected.to validate_presence_of :title }
