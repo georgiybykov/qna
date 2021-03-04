4030a4952f7d3a74a88e669b2a9632f4c3e4e157
georgiybykov <zhora.bykov@gmail.com>
2021-03-04 15:08:41 +0300

    Add spec for the author checkness of the object for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
-describe User, type: :model do
+describe User, type: :model, aggregate_failures: true do
+  let(:user) { create(:user) }
+
   it { is_expected.to have_many(:questions).dependent(:destroy) }
   it { is_expected.to have_many(:answers).dependent(:destroy) }
 
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_presence_of :password }
+
+  describe '#author?' do
+    subject(:result) { user.author?(object) }
+
+    context 'when the current user is not an author of the `object`' do
+      let(:object) { create(:question) }
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'when the current user is an author of the `object`' do
+      let(:object) { create(:answer, user: user) }
+
+      it { is_expected.to eq(true) }
+    end
+  end
 end
