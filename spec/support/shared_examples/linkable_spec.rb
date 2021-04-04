8eaa08d40c875f840f7a5f6b3e547ae12204e5ec
georgiybykov <zhora.bykov@gmail.com>
2021-04-05 00:45:36 +0300

    Update the show view for the question

--- spec/support/shared_examples/linkable_spec.rb
+++ spec/support/shared_examples/linkable_spec.rb
@@ -2,5 +2,5 @@
 
 shared_examples 'linkable' do
   it { is_expected.to have_many(:links).dependent(:destroy) }
-  it { is_expected.to accept_nested_attributes_for(:links) }
+  it { is_expected.to accept_nested_attributes_for(:links).allow_destroy(true) }
 end
