58f2f6453aadaea54c5fce476e4572b46047a360
Georgiy Bykov <zhora.bykov@gmail.com>
2021-06-07 03:00:13 +0300

    Add admin identifier for users

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_424_002_754) do
+ActiveRecord::Schema.define(version: 20_210_606_235_800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 20_210_424_002_754) do
     t.datetime 'confirmed_at'
     t.datetime 'confirmation_sent_at'
     t.string 'unconfirmed_email'
+    t.boolean 'admin', default: false
     t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
