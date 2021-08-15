38922b0e301ed7275db60c43f1320db616876c57
Georgiy Bykov <zhora.bykov@gmail.com>
2021-08-16 01:01:30 +0300

    Remove useless presence validations for Subscription associations

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -4,9 +4,6 @@ describe Subscription, type: :model, aggregate_failures: true do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:question) }
 
-  it { is_expected.to validate_presence_of :user }
-  it { is_expected.to validate_presence_of :question }
-
   it { is_expected.to have_db_column(:user_id).of_type(:integer).with_options(null: false) }
   it { is_expected.to have_db_column(:question_id).of_type(:integer).with_options(null: false) }
 
