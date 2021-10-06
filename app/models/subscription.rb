0f925b01f6dae11a2fe62c3ec59a1d8f99a14f00
Georgiy Bykov <zhora.bykov@gmail.com>
2021-10-06 04:03:38 +0300

    Chaching for show question page

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@
 
 class Subscription < ApplicationRecord
   belongs_to :user
-  belongs_to :question
+  belongs_to :question, touch: true
 
   validates :user, uniqueness: { scope: :question_id }
 end
